1664c4ff0660b44bd647c7ed678227350a3db321
TuanAnh <anhn58022@gmail.com>
2025-06-28 00:44:42 +0700

    Extra Assignment 4_VN.sql

--- Lesson 4/Extra Assignment 4_VN.sql
+++ Lesson 4/Extra Assignment 4_VN.sql
@@ -12,8 +12,8 @@ inner join
 	productsubcategory ps on p.ProductSubcategoryID = ps.ProductSubcategoryID
 where ps.name in (
 	select name 
-    from productsubcategory 
-    where name = 'Saddles'
+    	from productsubcategory 
+    	where name = 'Saddles'
 );
 
 -- Question 2: Thay đổi câu Query 1 để lấy được kết quả sau.
@@ -26,8 +26,8 @@ inner join
 	productsubcategory ps on p.ProductSubcategoryID = ps.ProductSubcategoryID
 where ps.name in (
 	select name
-    from productsubcategory
-    where name like 'Bo%'
+    	from productsubcategory
+    	where name like 'Bo%'
 );
 
 -- Question 3: Viết câu query trả về tất cả các sản phẩm có giá rẻ nhất (lowest ListPrice) và Touring Bike (nghĩa là ProductSubcategoryID = 3)
@@ -37,21 +37,24 @@ from
 	product 
 where ListPrice = (
 	select min(ListPrice) 
-    from product 
-    where ProductSubcategoryID = 3
+    	from product 
+    	where ProductSubcategoryID = 3
 );
 
 -- Exercise 2: JOIN nhiều bảng
 
 -- Question 1: Viết query lấy danh sách tên country và province được lưu trong AdventureWorks2008sample database.
 SELECT 
-    sp.StateProvinceID as STT, cr.CountryRegionCode AS `Country`, cr.name as `Countryname`, sp.name AS `Province`
+    sp.StateProvinceID as STT, 
+    cr.CountryRegionCode AS `Country`, 
+    cr.name as `Countryname`, 
+    sp.name AS `Province`
 FROM
     stateprovince sp
 INNER JOIN
     countryregion cr on sp.CountryRegionCode = cr.CountryRegionCode
 ORDER BY 
-	sp.StateProvinceID asc;
+    sp.StateProvinceID asc;
 
 -- Question 2: Tiếp tục với câu query trước và thêm điều kiện là chỉ lấy country Germany và Canada
 
